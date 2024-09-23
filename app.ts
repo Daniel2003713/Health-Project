f2103acf7403002253516ec322d17de3d851c70f
daniel <daniel.crazy_man@outlook.com>
2024-09-23 09:46:34 -0700

    app.ts created

--- app.ts
+++ app.ts
@@ -5,3 +5,24 @@ let heading = document.createElement('h1');
 heading.textContent = message;
 
 document.body.appendChild(heading);
+
+const fetchData = async (url: string): Promise<unknown> => {
+    const response = await fetch(url);
+    return await response.json();
+  };
+  
+  const showPosts = async () => {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    try {
+      const posts = await fetchData(url); // unknown type
+  
+      (
+        posts as { userId: number; id: number; title: string; body: string }[]
+      ).map((post) => console.log(post.title));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
+  showPosts();
+  
